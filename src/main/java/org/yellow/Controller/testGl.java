139e48c785b12d11916bf10fa382752906532418
黄世哲 <huangshizhe@meituan.com>
2018-01-08 13:56:48 +0800

    代码优化

--- src/main/java/org/yellow/Controller/testGl.java
+++ src/main/java/org/yellow/Controller/testGl.java
@@ -10,18 +10,21 @@ import javax.swing.*;
 
 public class testGl extends JFrame implements GLEventListener {
 
+    @Override
     public void init(GLAutoDrawable glAutoDrawable) {
 
     }
-
+    @Override
     public void dispose(GLAutoDrawable glAutoDrawable) {
 
     }
 
+    @Override
     public void display(GLAutoDrawable glAutoDrawable) {
 
     }
 
+    @Override
     public void reshape(GLAutoDrawable glAutoDrawable, int i, int i1, int i2, int i3) {
 
     }
